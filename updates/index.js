961262e757e09fc4529e31d1771f66f44417c67c
i32Designs <152013017+i32Designs@users.noreply.github.com>
2024-04-14 18:36:45 +0900

    Update index.js

--- updates/index.js
+++ updates/index.js
@@ -1,8 +1,8 @@
 // Example update data
 const updates = [
     { 
-        file: "バージョン 0.8.4（最新版）",
-        downloadLink: "https://drive.usercontent.google.com/download?id=1ls45cjqWbk08ntjmlJXcCmXQ9Laf6Crm&export=download&authuser=0",
+        file: "バージョン 0.8.4（Unreleased）",
+        downloadLink: "null",
         updateLink: "./info/0.8.4.html" 
     },
     { 
